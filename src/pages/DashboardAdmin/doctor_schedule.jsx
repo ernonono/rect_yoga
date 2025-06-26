52580ea84c9aaef0c4b2cda9951ba84ac00885be
ernonono <92450146+ernonono@users.noreply.github.com>
2025-06-26 14:03:36 +0700

    ekspor revisi

--- src/pages/DashboardAdmin/doctor_schedule.jsx
+++ src/pages/DashboardAdmin/doctor_schedule.jsx
@@ -35,13 +35,14 @@ function DoctorSchedule() {
 
   const handleDownloadData = async () => {
     try {
-      const response = await instance.get(
-        `/registrations-summary-schedule?start_date=${startDate}&end_date=${endDate}`,
-        {
-          responseType: "blob",
-        }
-      );
-
+      const response = await instance.get("/registrations-summary-schedule", {
+        responseType: "blob",
+        params: {
+          start_date: startDate,
+          end_date: endDate,
+        },
+      });
+      
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
